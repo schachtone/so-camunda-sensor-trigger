e46ae51d127032797170f491de1a1f6f1c8c9d65
Dominik Bial <dominik@dominiks-air.speedport_w_724v_typ_a_05011603_06_001>
2019-08-16 23:20:47 +0200

    added test file

--- endpoints/test.py
+++ endpoints/test.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(4,GPIO.OUT)
+GPIO.setup(7,GPIO.OUT)
 print "LED on"
 GPIO.output(4,GPIO.HIGH)
-time.sleep(5)
+time.sleep(7)
 print "LED off"
-GPIO.output(4,GPIO.LOW)
+GPIO.output(7,GPIO.LOW)
